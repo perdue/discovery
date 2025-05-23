4e6e34306c1be02b6a54709010b7da63d25a2d7a
Brent Perdue <perdue@users.noreply.github.com>
2025-05-23 15:19:05 -0400

    Capture progress

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,17 +3,55 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
-    let mut _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    // infinite loop; just so we don't leave this stack frame
+    let mut led = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let N = 5;
     loop {
-        _y = x;
+        let mut row;
+        let mut col;
+        for i in 0..16 {
+            if i < N {
+                row = 0;
+                col = i;
+                if col > 0 {
+                    led[row][col-1] = 0;
+                }
+                led[row][col] = 1;
+            } else if N < i && i < 10 {
+                row = (i-N)%N;
+                col = N - 1;
+                if row > 0 {
+                    led[row-1][col] = 0;
+                }
+                led[row][col] = 1;
+            }
+
+            display.show(&mut timer, led, 100);
+
+            display.clear();
+
+            timer.delay_ms(100_u32);
+        }
     }
 }
